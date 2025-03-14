a8702aa73161355cd109f30b45a28f75bb237fff
Yifannnnnnnnnn <yifan2sf@gmail.com>
2025-03-14 12:27:10 +0100

    tokenize the text

--- Part1FakeNews.py
+++ Part1FakeNews.py
@@ -1,5 +1,8 @@
 # Part 1
+## Task 1
 import re
+import nltk
+from nltk.tokenize import word_tokenize
 
 def clean_text(text):
     #Replaces text to be lowercase.
@@ -7,15 +10,21 @@ def clean_text(text):
     #Removing spaces/lines.
     text = re.sub(r'\s+', ' ', text)
     #Replacing numbers with NUM.
-    text = re.sub(r'\d+', '<NUM>', text)
+    text = re.sub(r'\d+', 'NUM', text)
     #Replacing dates with DATE.
-    text = re.sub(r'\d{4}-\d{2}-\d{2}', '<DATE>', text)
-    text = re.sub(r'\d{2}/\d{2}/\d{4}', '<DATE>', text)
+    text = re.sub(r'\d{4}-\d{2}-\d{2}', 'DATE', text)
+    text = re.sub(r'\d{2}/\d{2}/\d{4}', 'DATE', text)
     #Replacing emails with EMAIL.
-    text = re.sub(r'\S+@\S+\.\S+', '<EMAIL>', text)
+    text = re.sub(r'\S+@\S+\.\S+', 'EMAIL', text)
     #Replacing urls with URL.
-    text = re.sub(r'https?://\S+|www\.\S+', '<URL>', text)
-    return text
+    text = re.sub(r'https?://\S+|www\.\S+', 'URL', text)
+    
+
+    # Tokenization: Opdeling af teksten i ord
+    tokens = word_tokenize(text)
+    
+    # Returnerer tokens som en string, så de kan gemmes i CSV
+    return "[" + ", ".join(f"'{token}'" for token in tokens) + "]"  
 
 import pandas as pd
 
@@ -32,4 +41,3 @@ dataframe.to_csv("cleaned_dataset.csv", index=False)
 #Part 2
 #Part 3
 #Part 4
-#test
