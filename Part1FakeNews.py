12db63a4183ec11e3ca1a5f58096d74b9886ef51
PaddeFanger47 <martin2002.privat@gmail.com>
2025-03-28 13:09:59 +0100

    AS

--- Part1FakeNews.py
+++ Part1FakeNews.py
@@ -81,24 +81,34 @@ print(f"Reduktionshastighed efter stopord: {stopword_reduction:.2f}%")
 print(f"Ordforrådsstørrelse efter stemming: {vocab_size_stemmed}")
 print(f"Reduktionshastighed efter stemming: {stemming_reduction:.2f}%")
 
-#print("Stopwords liste:", stopwords)
-#print("Antal stopord i listen:", len(stop_words))
-
-#print("Før stopword fjernes:", word_tokenize(full_text.lower())[:50])
-#print("Efter stopword fjernes:", clean_text(full_text, remove_stopwords=True, apply_stemming=False).split()[:50])
-
 # Læs CSV-fil
 #file_path = r"C:\Users\yifan\Downloads\995,000_rows (1).csv"
+file_path = r"C:\Users\marti\OneDrive\Skrivebord\995000_rows.csv"
 
+print("Trying to start to read chunks:")
+#Ny måde, hvor vi prøver, at indlæse dokumentet lidt af gangen.
+chunk_size = 10000  # Læs 10.000 rækker ad gangen
 
+# Gem forbehandlede data
+output_file = "processed_995K_FakeNewsCorpus.csv"
+first_chunk = True
+Times = 0
+
+for chunk in pd.read_csv(file_path, chunksize=chunk_size, usecols=["content"], low_memory=False):
+   chunk["processed_text"] = chunk["content"].astype(str).apply(clean_text)
+   
+   mode = "w" if first_chunk else "a"
+
+   chunk.to_csv(output_file, mode=mode, header=first_chunk, index=False)
+
+   first_chunk = False
+   Times += len(chunk)
+   print(f"{Times}")
+print("Færdig")
 
 # task 3
 
-<<<<<<< Updated upstream
-import pandas as pd
-=======
 # Importerer nye libaries til counting, plotting, warnings og 
->>>>>>> Stashed changes
 import ast
 from collections import Counter
 import matplotlib.pyplot as plt
