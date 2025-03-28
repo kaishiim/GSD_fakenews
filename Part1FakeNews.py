66323a6e4121d8905e1bbced8ea49581118d253c
Yifannnnnnnnnn <yifan2sf@gmail.com>
2025-03-28 12:46:33 +0100

    task 3

--- Part1FakeNews.py
+++ Part1FakeNews.py
@@ -90,34 +90,7 @@ print(f"Reduktionshastighed efter stemming: {stemming_reduction:.2f}%")
 # Læs CSV-fil
 #file_path = r"C:\Users\yifan\Downloads\995,000_rows (1).csv"
 
-file_path = r"C:\Users\marti\OneDrive\Skrivebord\995000_rows.csv"
 
-#df = pd.read_csv(file_path, low_memory=False)
-
-#df_cleaned = df.map(lambda x: clean_text(str(x)))
-
-#print(df_cleaned.head(10))
-print("Trying to start to read chunks:")
-#Ny måde, hvor vi prøver, at indlæse dokumentet lidt af gangen.
-chunk_size = 10000  # Læs 10.000 rækker ad gangen
-
-# Gem forbehandlede data
-output_file = "processed_995K_FakeNewsCorpus.csv"
-first_chunk = True
-Times = 0
-
-for chunk in pd.read_csv(file_path, chunksize=chunk_size, usecols=["content"], low_memory=False):
-    chunk["processed_text"] = chunk["content"].astype(str).apply(clean_text)
-
-    mode = "w" if first_chunk else "a"
-
-    chunk.to_csv(output_file, mode=mode, header=first_chunk, index=False)
-
-    first_chunk = False
-    Times += len(chunk)
-    print(f"{Times}")
-
-print(f"Færdig")
 
 # task 3
 
